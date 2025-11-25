9b7851b8248899986fdd3cd95ce2ee039c5db492
EbraGory33 <96397436+EbraGory33@users.noreply.github.com>
2025-11-25 09:41:42 -0500

    remove Home component and update exports in index.ts

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,4 @@
 export { default as Landing } from "./Landing";
-export { default as Home } from "./Home";
 export { default as Login } from "./Auth/Login";
 export { default as Register } from "./Auth/Register";
 export { default as Explorer } from "./Explorer";
